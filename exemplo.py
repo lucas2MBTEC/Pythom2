4f4389f2c91c7858d3008c0b139b8a89038b6beb
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-02-12 11:04:30 -0300

    zddth

---
+++ exemplo.py
@@ -0,0 +1,14 @@
+#variavel é um nome que você da para algum valor ou alguma coisa especifica, evita ter que dígitar todas as vezes que precisar dígitar algo (este é um comentário)
+faturamento = 1000
+custo = 700
+lucro = faturamento - custo
+print ("o faturamento foi de", 1000)
+print ("o custo foi de", 700)
+print("o lucro foi de", 1000-700)
+# as aspas só serve para escrever textos e inserir virgula (este é um cometátio)
+#int números inteiros 
+#variavel booleano, descobre si é verdadeiro ou falso, true, false
+#números com casa decimal flat ou números quebrados
+
+
+
