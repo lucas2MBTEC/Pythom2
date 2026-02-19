64b15a7d4412c772f069ad75552868fb3e4c57fe
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-02-19 09:10:38 -0300

    Update exemplo.py
    lpkjuyi

--- exemplo.py
+++ exemplo.py
@@ -9,6 +9,8 @@ print("o lucro foi de", 1000-700)
 #int números inteiros 
 #variavel booleano, descobre si é verdadeiro ou falso, true, false
 #números com casa decimal flat ou números quebrados
-
-
-
+print(" a margem do lucro foi de"), margem_lucro
+#porcentagem de 10% no python é 0.1
+print("email@gmail.com")
+#roude é uma fumção de arredondar
+margem/lucro
